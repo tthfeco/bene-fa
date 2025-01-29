b75e3e8115d47734ccee24c22eef9e6be631ff6b
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2025-01-29 14:57:09 +0100

    save to continue

--- bene-fa/src/App.jsx
+++ bene-fa/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Categories from "./components/Categories";
 
-function App() {
+function App() {  
   return (
     <div style={{ margin: 0, padding: 0, overflowX: "hidden", width: "100vw" }}>
       <Header />
