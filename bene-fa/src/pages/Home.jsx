7d6d2af9a8e92ede8b63bc3423fe1aaaf7b71941
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2025-01-29 11:54:54 +0100

    create basic prjoect strucure

---
+++ bene-fa/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Üdv a Bene-Fa Kft. weboldalán!</h1>
+      <p>Ez egy React alapú alkalmazás, amelyet GitHub Pages-en hostolunk.</p>
+    </div>
+  );
+}
