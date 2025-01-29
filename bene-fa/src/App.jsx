c545d620bc8e64685d15edc69d216a2558bec89e
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2025-01-29 14:19:57 +0100

    add categories to app.jsx

--- bene-fa/src/App.jsx
+++ bene-fa/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Categories from "./components/Categories";
 
 function App() {
   return (
-    <div style={{ margin: 0, padding: 0, overflowX: "hidden", width: "100vw" }}>
+    <div style={{ margin: 0, padding: 0, overflowX: "hidden" }}>
       <Header />
       <Banner />
+      <Categories />
     </div>
   );
 }
