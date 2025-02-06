7079a6aa568e24c74b8ea73dd2c37567b95fb412
Tóth Ferenc <fec@PC192.168.1.67>
2025-02-06 15:01:02 +0100

    add line between the gallery

--- bene-fa/src/App.jsx
+++ bene-fa/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Categories from "./components/Categories";
+import ReferenceComponent from "./components/Referencia.jsx";
 
 function App() {  
   return (
@@ -9,6 +10,7 @@ function App() {
       <Banner />      
       <div style={{ height: "100vh" }}></div> {/* Üres hely a banner után */}
       <Categories />
+      <ReferenceComponent />
       <div style={{ height: "100vh" }}></div> {/* Üres hely a banner után */}
     </div>
   );
---
+++ bene-fa/src/components/Referencia.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ReferenceComponent.css';
+
+const ReferenceComponent = () => {
+  return (
+    <div className="reference-container">
+      <span className="reference-title">REFERENCIA MUNKÁINK</span>
+      <div className="reference-line-container">
+        <span className="reference-number">02/24</span>
+        <div className="reference-line"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ReferenceComponent;
