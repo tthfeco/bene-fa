776a4345fda2cf68f8f8b547ab0a713ff56408bd
Tóth Ferenc <tth.feco@gmail.com>
2025-02-26 14:18:09 +0100

    set hamburge menu to header

--- bene-fa/src/components/Header.jsx
+++ bene-fa/src/components/Header.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./Header.css";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header>
       <div className="logo">BENE-FA KFT.</div>
-      <nav className="nav"> {}
+
+      {/* Hamburger ikon */}
+      <div className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Navigációs menü, amely aktív osztályt kap ha a menü nyitva van */}
+      <nav className={`nav ${menuOpen ? "active" : ""}`}>
         <a href="#">Főoldal</a>
         <a href="#">Kapcsolat</a>
         <a href="#">Rólunk</a>
