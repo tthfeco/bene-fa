1af71a3b66a260627080ab77d1b4a45cdc7e94cf
Tóth Ferenc <tth.feco@gmail.com>
2025-04-17 11:07:49 +0200

    create BelteriAjto jsx

---
+++ bene-fa/src/pages/BelteriAjto.jsx
@@ -0,0 +1,9 @@
+export default function BelteriAjto() {
+    return (
+      <div className="belteri-ajto-page">
+        <h1>Beltéri ajtók</h1>
+        <p>Itt jelennek meg majd a beltéri ajtókkal kapcsolatos információk, képek, leírások stb.</p>
+      </div>
+    );
+  }
+  
