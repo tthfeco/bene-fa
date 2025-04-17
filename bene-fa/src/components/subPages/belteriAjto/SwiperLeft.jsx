0aeaf0901370b2c9a028c194a9e3e8c9ed2a1c0c
Tóth Ferenc <tth.feco@gmail.com>
2025-04-17 12:49:23 +0200

    move another folder the door pictures/files

---
+++ bene-fa/src/components/subPages/belteriAjto/SwiperLeft.jsx
@@ -0,0 +1,10 @@
+import "./SwiperLeft.css";
+
+export default function SwiperLeft() {
+  return (
+    <div className="swiper-left-wrapper">
+      <div className="leftContent"></div>
+      <div className="rightContent"></div>
+    </div>
+  );
+}
