37fcee00f4f95a93b1aa292c177812afc500e3d3
Tóth Ferenc <tth.feco@gmail.com>
2025-06-11 11:26:50 +0200

    Add RightText component markup

--- bene-fa/src/components/subPages/belteriAjto/RightText.jsx
+++ bene-fa/src/components/subPages/belteriAjto/RightText.jsx
@@ -1,4 +1,26 @@
+import "./RightText.css";
+
 export default function RightText() {
-    return <div>Right Text here</div>;
-  };
-  
+  return (
+    <div className="right-text">
+      <h2>AJTÓ</h2>
+      <p className="subtitle">beltéri, tolóajtó</p>
+
+      <p><strong>Anyag:</strong> borovifenyő 3 rétegben ragasztott, hossz­toldott fríz</p>
+      <p><strong>Betét:</strong> nyár rétegelt lemez <em>(RAL színek esetében)</em>; borovifenyő színfurnéros rétegelt lemez <strong>(vastag lazúr esetében)</strong>; üveg <em>(mintázat igény szerint választható)</em></p>
+      <p><strong>Méretek:</strong> egyedi méretre gyártás, fal vastagságra gyártott utólag beépíthető tokkal</p>
+
+      <p><strong>Vasalatok:</strong></p>
+      <ul>
+        <li>3 dimenziós pánttal szerelve</li>
+        <li>MIDI 7 bevéső zárral <em>(kulcslyukas, wc záras)</em></li>
+        <li>tolóajtó vasalat</li>
+      </ul>
+
+      <p><strong>Kilincs:</strong> katalógusból választható</p>
+      <p><strong>Szín:</strong> vastag lazúr vagy RAL színkártya alapján választható <em>(fa erezet nem látszódik)</em></p>
+      <p><strong>Vastag lazúr színkód:</strong></p>
+      <p>XHT 113 1 réteg, XBC 2 1 réteg, XGC 068 K 1 réteg</p>
+    </div>
+  );
+}
