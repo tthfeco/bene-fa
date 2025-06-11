e02847e35cb1436ddd55d55517dbef1b05512c84
Tóth Ferenc <tth.feco@gmail.com>
2025-06-11 13:33:32 +0200

    Enable color selection and store active color globally

--- bene-fa/src/components/subPages/belteriAjto/ColorSelector.jsx
+++ bene-fa/src/components/subPages/belteriAjto/ColorSelector.jsx
@@ -1,26 +1,28 @@
 import "./ColorSelector.css";
 import doorColors from "../../../data/belteri/doorColors";
+import { useActiveDoorColor } from "../../../context/ActiveDoorColorContext";
 
 export default function ColorSelector() {
+  const { activeColorId, setActiveColorId } = useActiveDoorColor();
+
   const firstRow = doorColors.slice(0, 8);
   const secondRow = doorColors.slice(8);
 
+  const renderRow = (row) =>
+    row.map((color) => (
+      <div
+        key={color.id}
+        className={`color-option ${activeColorId === color.id ? "active" : ""}`}
+        onClick={() => setActiveColorId(color.id)}
+      >
+        <img src={color.image} alt={`Color ${color.id}`} />
+      </div>
+    ));
+
   return (
     <div className="color-selector">
-      <div className="color-row">
-        {firstRow.map((color) => (
-          <div key={color.id} className="color-option">
-            <img src={color.image} alt={`Color ${color.id}`} />
-          </div>
-        ))}
-      </div>
-      <div className="color-row">
-        {secondRow.map((color) => (
-          <div key={color.id} className="color-option">
-            <img src={color.image} alt={`Color ${color.id}`} />
-          </div>
-        ))}
-      </div>
+      <div className="color-row">{renderRow(firstRow)}</div>
+      <div className="color-row">{renderRow(secondRow)}</div>
     </div>
   );
 }
