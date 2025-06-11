f023b3e6a255e9a712f4e329788672e299e752cf
Tóth Ferenc <tth.feco@gmail.com>
2025-06-11 13:31:43 +0200

    Wrap App with ActiveDoorColorProvider in main.jsx

--- bene-fa/src/main.jsx
+++ bene-fa/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ActiveDoorProvider } from './context/ActiveDoorContext.jsx';
+import { ActiveDoorColorProvider } from './context/ActiveDoorColorContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ActiveDoorProvider>
-      <App />
+      <ActiveDoorColorProvider>
+        <App />
+      </ActiveDoorColorProvider>
     </ActiveDoorProvider>
   </React.StrictMode>
 );
