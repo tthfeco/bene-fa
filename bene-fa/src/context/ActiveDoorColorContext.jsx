db78589bbf7e9db946af5f79cb852516216044a5
Tóth Ferenc <tth.feco@gmail.com>
2025-06-11 13:36:08 +0200

    Complete active color selection with global context and styling

--- bene-fa/src/context/ActiveDoorColorContext.jsx
+++ bene-fa/src/context/ActiveDoorColorContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const ActiveDoorColorContext = createContext();
 
 export function ActiveDoorColorProvider({ children }) {
-  const [activeColorId, setActiveColorId] = useState("1");
+  const [activeColorId, setActiveColorId] = useState("2");
 
   return (
     <ActiveDoorColorContext.Provider value={{ activeColorId, setActiveColorId }}>
