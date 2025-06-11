3b9ba2592a0489a15f8513a74b4312e4cee6382b
Tóth Ferenc <tth.feco@gmail.com>
2025-06-11 12:03:09 +0200

    Add color image imports and mainColors array

---
+++ bene-fa/src/data/belteri/doorColors.js
@@ -0,0 +1,42 @@
+import color1 from "/src/assets/doors/belteri/colors/1.svg";
+import color2 from "/src/assets/doors/belteri/colors/2.svg";
+import color3 from "/src/assets/doors/belteri/colors/3.svg";
+import color4 from "/src/assets/doors/belteri/colors/4.svg";
+import color5 from "/src/assets/doors/belteri/colors/5.svg";
+import color6 from "/src/assets/doors/belteri/colors/6.svg";
+import color7 from "/src/assets/doors/belteri/colors/7.svg";
+import color8 from "/src/assets/doors/belteri/colors/8.svg";
+import color9 from "/src/assets/doors/belteri/colors/9.svg";
+import color10 from "/src/assets/doors/belteri/colors/10.svg";
+import color11 from "/src/assets/doors/belteri/colors/11.svg";
+import color12 from "/src/assets/doors/belteri/colors/12.svg";
+import color13 from "/src/assets/doors/belteri/colors/13.svg";
+import color14 from "/src/assets/doors/belteri/colors/14.svg";
+import color15 from "/src/assets/doors/belteri/colors/15.svg";
+import color16 from "/src/assets/doors/belteri/colors/16.svg";
+import color17 from "/src/assets/doors/belteri/colors/17.svg";
+import color18 from "/src/assets/doors/belteri/colors/18.svg";
+
+const mainColors = [
+  { id: '1', image: color1 },
+  { id: '2', image: color2 },
+  { id: '3', image: color3 },
+  { id: '4', image: color4 },
+  { id: '5', image: color5 },
+  { id: '6', image: color6 },
+  { id: '7', image: color7 },
+  { id: '8', image: color8 },
+  { id: '9', image: color9 },
+  { id: '10', image: color10 },
+  { id: '11', image: color11 },
+  { id: '12', image: color12 },
+  { id: '13', image: color13 },
+  { id: '14', image: color14 },
+  { id: '15', image: color15 },
+  { id: '16', image: color16 },
+  { id: '17', image: color17 },
+  { id: '18', image: color18 },
+];
+
+
+export default mainColors;
