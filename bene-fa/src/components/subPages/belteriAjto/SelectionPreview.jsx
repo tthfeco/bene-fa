a1a8193775a04d7844b36a3ce0f5d131e72cc842
Tóth Ferenc <tth.feco@gmail.com>
2025-06-12 09:06:41 +0200

    Add SelectionPreview component to display door-color image variants

--- bene-fa/src/components/subPages/belteriAjto/SelectionPreview.jsx
+++ bene-fa/src/components/subPages/belteriAjto/SelectionPreview.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { useActiveDoor } from "../../../context/ActiveDoorContext";
+import { useActiveDoorColor } from "../../../context/ActiveDoorColorContext";
+import "./SelectionPreview.css"; 
 
 export default function SelectionPreview() {
-    return <div>Preview Text here</div>;
-  };
-  
+  const { activeDoorId } = useActiveDoor();
+  const { activeColorId } = useActiveDoorColor();
+
+  if (!activeDoorId || !activeColorId) return null;
+
+  const suffixes = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+  return (
+    <div className="selection-preview">
+      {suffixes.map((suffix) => {
+        const src = `/src/assets/doors/belteri/coloredDoors/${activeDoorId}/${activeDoorId}_${activeColorId}_${suffix}.svg`;
+        return (
+          <img
+            key={suffix}
+            src={src}
+            alt={`Ajtó ${activeDoorId} - Szín ${activeColorId} - ${suffix}`}
+            onError={(e) => (e.target.style.display = "none")}
+            className="preview-image"
+          />
+        );
+      })}
+    </div>
+  );
+}
