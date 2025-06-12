451abc1d4e58d5eea68f669d69dd809d1b4e83bd
Tóth Ferenc <tth.feco@gmail.com>
2025-06-12 10:47:14 +0200

    ...

--- bene-fa/vite.config.js
+++ bene-fa/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/bene-fa/" : "/",
+  base: "/bene-fa/", // <<< pontosan ez KELL
 });
