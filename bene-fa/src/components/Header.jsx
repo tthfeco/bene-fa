ecd2d27f420e8ea23f1557805ace3eb6e400da23
Tóth Ferenc <tth.feco@gmail.com>
2025-06-13 12:22:22 +0200

    Implement animated X icon and styling for active hamburger menu

--- bene-fa/src/components/Header.jsx
+++ bene-fa/src/components/Header.jsx
@@ -9,13 +9,16 @@ export default function Header() {
       <div className="logo">BENE-FA KFT.</div>
 
       {/* Hamburger ikon */}
-      <div className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+      <div
+        className={`hamburger-menu ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
         <div></div>
         <div></div>
         <div></div>
       </div>
 
-      {/* Navigációs menü, amely aktív osztályt kap ha a menü nyitva van */}
+      {/* Navigációs menü */}
       <nav className={`nav ${menuOpen ? "active" : ""}`}>
         <a href="#">Főoldal</a>
         <a href="#">Kapcsolat</a>
