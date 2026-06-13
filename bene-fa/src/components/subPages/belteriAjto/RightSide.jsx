c64dd6bd87f88d1e2cfcb9144681b4bc4027ce64
Tóth Ferenc <tth.feco@gmail.com>
2026-06-13 10:34:04 +0200

    t

--- bene-fa/src/components/subPages/belteriAjto/RightSide.jsx
+++ bene-fa/src/components/subPages/belteriAjto/RightSide.jsx
@@ -1,13 +1,14 @@
 import RightText from "./RightText";
 import ColorSelector from "./ColorSelector";
 import SelectionPreview from "./SelectionPreview";
+import doorColors from "../../../data/belteri/doorColors";
 
-export default function RightSide() {
-    return (
-      <div className="right-side-content">
-        <RightText />
-        <ColorSelector />
-        <SelectionPreview />
-      </div>
-    );
-  };
+export default function RightSide({ colors, basePath, rightText }) {
+  return (
+    <div className="right-side-content">
+      {rightText || <RightText />}
+      <ColorSelector colors={colors || doorColors} />
+      <SelectionPreview basePath={basePath || "/assets/doors/belteri/coloredDoors"} />
+    </div>
+  );
+}
