282be1badb3b65ff9cbcadb16d80cd83048a46ab
Tóth Ferenc <tth.feco@gmail.com>
2026-06-13 10:49:38 +0200

    update

---
+++ bene-fa/src/components/subPages/kulteri/RightText.jsx
@@ -0,0 +1,22 @@
+import "../belteriAjto/RightText.css";
+
+export default function RightTextKulteri() {
+  return (
+    <div className="right-text">
+      <h2>AJTÓ</h2>
+      <p className="subtitle">kültéri, fa bejárati</p>
+      <p><strong>Anyag:</strong> borovifenyő 3 rétegben ragasztott, hossztoldott fríz</p>
+      <p><strong>Betét:</strong> nyár rétegelt lemez <em>(RAL színek esetében)</em>; borovifenyő színfurnéros rétegelt lemez <em>(vastag lazúr esetében)</em>; kétrétegű hőszigetelt üveg <em>(mintázat igény szerint választható)</em>; borovifenyő mart betét <em>(RAL szín, illetve vastag lazúr esetében)</em></p>
+      <p><strong>Méretek:</strong> egyedi méretre gyártás</p>
+      <p><strong>Vasalatok:</strong></p>
+      <ul>
+        <li>3 dimenziós pánttal szerelve</li>
+        <li>5 pontos zárral szerelve</li>
+      </ul>
+      <p><strong>Kilincs:</strong> katalógusból választható</p>
+      <p><strong>Szín:</strong> vastag lazúr vagy RAL színkártya alapján választható <em>(fa erezet nem látszódik)</em></p>
+      <p><strong>Vastag lazúr színkód:</strong></p>
+      <p>XHT 113 1 réteg, XBC 2 1 réteg, XGC 068 K 1 réteg</p>
+    </div>
+  );
+}
---
+++ bene-fa/src/data/kulteri/mainDoors.js
@@ -0,0 +1,47 @@
+import door0 from "/src/assets/doors/kulteri/0.svg";
+import door1 from "/src/assets/doors/kulteri/1.svg";
+import door2 from "/src/assets/doors/kulteri/2.svg";
+import door3 from "/src/assets/doors/kulteri/3.svg";
+import door4 from "/src/assets/doors/kulteri/4.svg";
+import door5 from "/src/assets/doors/kulteri/5.svg";
+import door6 from "/src/assets/doors/kulteri/6.svg";
+import door7 from "/src/assets/doors/kulteri/7.svg";
+import door8 from "/src/assets/doors/kulteri/8.svg";
+import door9 from "/src/assets/doors/kulteri/9.svg";
+import door10 from "/src/assets/doors/kulteri/10.svg";
+import door11 from "/src/assets/doors/kulteri/11.svg";
+import door12 from "/src/assets/doors/kulteri/12.svg";
+import door13 from "/src/assets/doors/kulteri/13.svg";
+import door14 from "/src/assets/doors/kulteri/14.svg";
+import door15 from "/src/assets/doors/kulteri/15.svg";
+import door16 from "/src/assets/doors/kulteri/16.svg";
+import door17 from "/src/assets/doors/kulteri/17.svg";
+import door18 from "/src/assets/doors/kulteri/18.svg";
+import door19 from "/src/assets/doors/kulteri/19.svg";
+import door20 from "/src/assets/doors/kulteri/20.svg";
+
+const mainDoors = [
+  { id: "0", image: door0 },
+  { id: "1", image: door1 },
+  { id: "2", image: door2 },
+  { id: "3", image: door3 },
+  { id: "4", image: door4 },
+  { id: "5", image: door5 },
+  { id: "6", image: door6 },
+  { id: "7", image: door7 },
+  { id: "8", image: door8 },
+  { id: "9", image: door9 },
+  { id: "10", image: door10 },
+  { id: "11", image: door11 },
+  { id: "12", image: door12 },
+  { id: "13", image: door13 },
+  { id: "14", image: door14 },
+  { id: "15", image: door15 },
+  { id: "16", image: door16 },
+  { id: "17", image: door17 },
+  { id: "18", image: door18 },
+  { id: "19", image: door19 },
+  { id: "20", image: door20 },
+];
+
+export default mainDoors;
