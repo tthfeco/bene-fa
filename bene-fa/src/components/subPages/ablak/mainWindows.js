9f678a9f070f08d8fa111de452018463ca162784
Tóth Ferenc <tth.feco@gmail.com>
2026-06-13 10:39:43 +0200

    potlas

--- bene-fa/src/components/subPages/ablak/mainWindows.js
+++
@@ -1,9 +0,0 @@
-import window0 from "/src/assets/doors/ablak/0.svg";
-import window1 from "/src/assets/doors/ablak/1.svg";
-
-const mainWindows = [
-  { id: "0", image: window0 },
-  { id: "1", image: window1 },
-];
-
-export default mainWindows;
