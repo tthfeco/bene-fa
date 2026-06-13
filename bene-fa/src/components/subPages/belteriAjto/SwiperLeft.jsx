029bce5964ba4c9b50ab6f0b5b78e91793a5537a
Tóth Ferenc <tth.feco@gmail.com>
2026-06-13 10:25:18 +0200

    refactor: make door components reusable with props

--- bene-fa/src/components/subPages/belteriAjto/SwiperLeft.jsx
+++ bene-fa/src/components/subPages/belteriAjto/SwiperLeft.jsx
@@ -1,58 +1,48 @@
 import { useState, useEffect } from "react";
 import "./SwiperLeft.css";
-import mainDoors from "../../../data/belteri/mainDoors.js";
 import { useActiveDoor } from "../../../context/ActiveDoorContext.jsx";
 
-export default function SwiperLeft() {
+export default function SwiperLeft({ items }) {
   const [startIndex, setStartIndex] = useState(0);
   const { setActiveDoorId } = useActiveDoor();
 
   useEffect(() => {
-    const centerIndex = (startIndex + 1) % mainDoors.length;
-    setActiveDoorId(mainDoors[centerIndex].id);
+    const centerIndex = (startIndex + 1) % items.length;
+    setActiveDoorId(items[centerIndex].id);
   }, [startIndex]);
 
   const handlePrev = () => {
-    setStartIndex((prev) =>
-      (prev - 1 + mainDoors.length) % mainDoors.length
-    );
+    setStartIndex((prev) => (prev - 1 + items.length) % items.length);
   };
 
   const handleNext = () => {
-    setStartIndex((prev) => (prev + 1) % mainDoors.length);
+    setStartIndex((prev) => (prev + 1) % items.length);
   };
 
   const visibleDoors = [
-    mainDoors[startIndex % mainDoors.length],
-    mainDoors[(startIndex + 1) % mainDoors.length],
-    mainDoors[(startIndex + 2) % mainDoors.length],
+    items[startIndex % items.length],
+    items[(startIndex + 1) % items.length],
+    items[(startIndex + 2) % items.length],
   ];
 
-  const activeIndex = (startIndex + 1) % mainDoors.length;
+  const activeIndex = (startIndex + 1) % items.length;
 
   return (
     <div className="swiper-left-wrapper">
-  <div className="swiper-gallery">
-    <button className="arrow up" onClick={handlePrev}>↑</button>
-
-    <div className="swiper-images">
-      {visibleDoors.map((door, index) => (
-        <div
-          key={door.id}
-          className={`swiper-item ${index === 1 ? "active" : ""}`}
-        >
-          <img src={door.image} alt={`Ajtó ${door.id}`} />
+      <div className="swiper-gallery">
+        <button className="arrow up" onClick={handlePrev}>↑</button>
+        <div className="swiper-images">
+          {visibleDoors.map((door, index) => (
+            <div key={door.id} className={`swiper-item ${index === 1 ? "active" : ""}`}>
+              <img src={door.image} alt={`${door.id}`} />
+            </div>
+          ))}
         </div>
-      ))}
+        <button className="arrow down" onClick={handleNext}>↓</button>
+      </div>
+      <div className="swiper-counter">
+        {activeIndex + 1}/{items.length}
+      </div>
     </div>
-
-    <button className="arrow down" onClick={handleNext}>↓</button>
-  </div>
-
-  <div className="swiper-counter">
-    {activeIndex + 1}/{mainDoors.length}
-  </div>
-</div>
-
   );
 }
--- bene-fa/src/pages/BelteriAjto.jsx
+++ bene-fa/src/pages/BelteriAjto.jsx
@@ -3,20 +3,21 @@ import SubHeader from "../components/SubHeader";
 import LargePreview from "../components/subPages/belteriAjto/LargePreview.jsx";
 import SwiperLeft from "../components/subPages/belteriAjto/SwiperLeft";
 import RightSide from "../components/subPages/belteriAjto/RightSide.jsx";
+import mainDoors from "../data/belteri/mainDoors.js";
 
 export default function BelteriAjto() {
   return (
     <div className="interior-door-page">
       <SubHeader />
       <div className="interior-door-container">
-      <div className="leftSide">
-        <div className="leftSide-inner">
-            <LargePreview />
-            <SwiperLeft />
-        </div>  
+        <div className="leftSide">
+          <div className="leftSide-inner">
+            <LargePreview items={mainDoors} />
+            <SwiperLeft items={mainDoors} />
+          </div>
         </div>
         <div className="rightSide">
-        <RightSide />
+          <RightSide />
         </div>
       </div>
     </div>
